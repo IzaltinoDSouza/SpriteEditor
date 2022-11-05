719cc0ff2e6cd67cab10953d8b51947f4d90dc6f
izaltinodsouza <izaltinodsouza@gmail.com>
2022-11-05 04:32:16 -0300

    change tabs indentation to spaces

--- editor.h
+++ editor.h
@@ -11,16 +11,16 @@ typedef struct
     
     struct Window
     {
-    	size_t width;
-    	size_t height; 
+        size_t width;
+        size_t height; 
     }window;
     
     struct SpriteDocWindow
     {
-    	size_t x;
-    	size_t y;
-    	size_t width;
-    	size_t height;
+        size_t x;
+        size_t y;
+        size_t width;
+        size_t height;
     }sprite_doc_window;
 
     struct ColorsWindow
