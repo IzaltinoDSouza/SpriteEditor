581091c873ec28e507c0c2bafcff95b3beca351f
izaltinodsouza <izaltinodsouza@gmail.com>
2022-11-05 01:24:57 -0300

    add SpriteDoc in main,and change sprite_editor_load_sprite from sprite to sprite_doc

--- main.c
+++ main.c
@@ -3,19 +3,24 @@
 
 #include "sprite.h"
 #include "editor.h"
+#include "sprite_doc.h"
 
 int main()
 {
-	Sprite test = sprite_load("demo.sprite");
-	
-	SpriteEditor * editor = sprite_editor_init("Sprite Editor",512,512,16);
-	
-	sprite_editor_load_sprite(editor,&test);
-	
-	sprite_editor_draw_sprite(editor,0,0);
-	//sprite_editor_render_sprite(editor,0,0);
-	
-	sprite_editor_event_loop(editor);
-	
-	sprite_editor_exit(editor);
+    Sprite test = sprite_load("demo.sprite");
+
+    SpriteDoc doc = sprite_doc_create_from_sprite("enemy","atsindev",&test);
+
+    SpriteEditor * editor = sprite_editor_init("Sprite Editor",512,512,16);
+
+    sprite_editor_load_sprite(editor,&doc);
+
+    sprite_editor_draw_sprite(editor,0,0);
+    //sprite_editor_render_sprite(editor,0,0);
+
+    sprite_editor_event_loop(editor);
+
+    sprite_editor_exit(editor);
+
+    sprite_doc_clearup(&doc);
 }
