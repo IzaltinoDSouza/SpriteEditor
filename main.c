defbadb5211c7adcff73a983db7346f2692ea4b3
izaltinodsouza <izaltinodsouza@gmail.com>
2022-11-07 02:39:38 -0300

    SpriteEditor open file by commandline

--- main.c
+++ main.c
@@ -5,11 +5,16 @@
 #include "editor.h"
 #include "sprite_doc.h"
 
-int main()
+int main(int argc,char **argv)
 {
+    if(argc < 2)
+    {
+        puts("SpriteEditor FILENAME.sprite");
+        exit(1);
+    }
     SpriteEditor * editor = sprite_editor_init("Sprite Editor",512,512,16);
 
-    sprite_editor_load_sprite_from_disk(editor,"demo.sprite");
+    sprite_editor_load_sprite_from_disk(editor,argv[1]);
 
     sprite_editor_draw_sprite(editor,0,0);
     //sprite_editor_render_sprite(editor,0,0);
